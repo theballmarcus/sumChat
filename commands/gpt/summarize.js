9bec224ee2d2fcb21ebfd47d86a1de280d49b9e1
Marcus Christian Ihme <mci001@politi.dk>
2023-04-01 16:20:55 +0200

    Added a command argument for how many messages

--- commands/gpt/summarize.js
+++ commands/gpt/summarize.js
@@ -5,20 +5,23 @@ const chatApiKey = require('../../config.json')["chatApiKey"]
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('summarize')
-		.setDescription('Summarizes n amount of messages or minutes in chat. Default: messages'),
-        
+		.setDescription('Summarizes n amount of messages in chat.')
+        .addStringOption(option =>
+			option
+				.setName('messages')
+				.setDescription('How many messages do you want to summarize?')
+		),
 	async execute(interaction) {
 		let sendPendingMessage = interaction.reply("Pending...").catch(error => {
 			console.log(error);
 		});
-		interaction.channel.messages.fetch({limit: 30}).then(contextMsgs => {
+		interaction.channel.messages.fetch({limit: interaction.options.getString('messages')}).then(contextMsgs => {
 		  const context = contextMsgs.map(msg => `${msg.member.user.tag}\n${msg.content}`).join('\n');
 		  const api = new ChatGPT(chatApiKey);
 		  askGPT = api.askGPT("Summarize what happened in this chat very shortly: \n" + context).catch(error => {
 			console.log(error);
 		  });
 		  Promise.all([askGPT, sendPendingMessage]).then(values => {
-			console.log("works")
 			values[1].edit(values[0].data.choices[0].message)
 		  }).catch(error => {
 			console.log(error);
